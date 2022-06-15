ccf00cbea41577a8485a0e6c3ab2156e7dfa355f
Recursive-Tahmal <100554398+Recursive-Tahmal@users.noreply.github.com>
2022-06-15 18:16:28 +0300

    Time: 46 ms (5.50%), Space: 42.3 MB (22.70%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,25 +1,12 @@
-import java.util.Stack;
-
-class Solution {
+public class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++) {
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '[' || ch == '{') {
-                st.push(ch);
-            } else {
-                if (st.isEmpty()) {
-                    return false;
-                } else if (!matching( st.peek() , ch)) {
-                    return false;
-                } else st.pop();
-            }
-        }
-        return (st.empty());
-
-    }
-
-    public static boolean matching(char a , char b){
-        return (a =='(' && b==')') || (a=='[' && b==']') || (a=='{' && b=='}') ;
+        int length;
+    
+        do {
+            length = s.length();
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        } while(length != s.length());
+    
+        return s.length() == 0;
     }
 }
