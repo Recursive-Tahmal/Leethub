db6d2db430753fb3ae775c535c9750b34c9abbf1
Recursive-Tahmal <100554398+Recursive-Tahmal@users.noreply.github.com>
2022-06-21 16:55:17 +0300

    Time: 98 ms (15.81%), Space: 44.6 MB (73.02%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,7 +6,6 @@ class Solution {
                 if(nums[i] + nums[j] == target){
                     answer[0] = i;
                     answer[1] = j;
-                    break;
                 }
             }
         }
