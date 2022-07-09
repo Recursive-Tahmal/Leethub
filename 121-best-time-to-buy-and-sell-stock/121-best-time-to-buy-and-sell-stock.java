5d5453a3bc29eacb9ae5e3c70b7843afbcfdc365
Recursive-Tahmal <100554398+Recursive-Tahmal@users.noreply.github.com>
2022-07-10 01:16:56 +0300

    Time: 1 ms (100.00%), Space: 59.1 MB (87.98%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int max = 0, min = prices[0];
-        for(int i = 1; i < prices.length; ++i){
+        for(int i = 0; i < prices.length; ++i){
             if(min<prices[i])
                 max = Math.max(prices[i]-min,max);
             else{
