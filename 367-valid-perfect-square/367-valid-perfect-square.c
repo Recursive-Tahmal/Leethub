0f0b4cce24132f077286d612acbe88f21c863c6f
Recursive-Tahmal <100554398+Recursive-Tahmal@users.noreply.github.com>
2022-07-27 15:58:52 +0300

    Time: 0 ms (100.00%), Space: 5.3 MB (91.94%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.c
@@ -0,0 +1,15 @@
+bool isPerfectSquare(int x){
+long num = 1;
+	while (num <= x)
+	{
+		if (num * num == x) {
+			return true;
+		}
+		else if (num * num > x) {
+			return false;
+		}
+
+		++num;
+	}
+	return false;
+}
